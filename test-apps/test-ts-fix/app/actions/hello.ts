e50a80dd9760629c6eb10f370db02338e9bb95ca
Soham Pramanik <clashersoham07@gmail.com>
2026-01-15 19:00:39 +0530

    clean up

--- test-apps/test-ts-fix/app/actions/hello.ts
+++
@@ -1,9 +0,0 @@
-interface HelloResponse {
-    message: string;
-}
-
-export const hello = defineAction((req): HelloResponse => {
-    return {
-        message: `Hello from Titan ${req.body.name || "World"}`,
-    };
-});
